2fb47ec9ef7e425a4798050cd6d8f35b31d30794
Pippin Williamson <pippin@pippinsplugins.com>
2014-03-12 09:58:18 -0500

    Move newsletter loop up and add post data t0 do_action

--- cgc-rcp-stripe-control.php
+++ cgc-rcp-stripe-control.php
@@ -84,15 +84,6 @@ function cgc_rcp_process_free_signup() {
 		update_user_meta( $user_id, 'rcp_subscription_level', 1 );
 		rcp_set_status( $user_id, 'free' );
 
-		do_action( 'cgc_rcp_account_created', $user_id );
-		
-		$creds = array();
-		$creds['user_login'] = $user_login;
-		$creds['user_password'] = $user_pass;
-		$creds['remember'] = false;
-		$user = wp_signon( $creds, false );
-
-
 		// Process newsletter signups
 		if( ! empty( $_POST['newsletters'] ) ) {
 			
@@ -158,6 +149,13 @@ function cgc_rcp_process_free_signup() {
 			}
 		}
 
+		do_action( 'cgc_rcp_account_created', $user_id, $_POST['newsletters'] );
+		
+		$creds = array();
+		$creds['user_login'] = $user_login;
+		$creds['user_password'] = $user_pass;
+		$creds['remember'] = false;
+		$user = wp_signon( $creds, false );
 
 		die('1');
 	} else {
